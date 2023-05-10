b77a97fba44840689291e4fbbcbbb28e91596c4b
aksel96 <jonhaksel@gmail.com>
2023-05-10 10:33:20 -0600

    Funcion producto, suma y resta, menu terminado

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
-#   AKSEL VILLELA ANDRADE
+#   AKSEL VILLELA ANDRADE, ARTURO IVAN AZUARA OCOTITLA, ELIEZER ISAI MONROY QUINTERO
 #   FES ARAGON
 #   ICO
 #   ALGEBRA LINEAL
 #   PROYECTO FINAL
 #   MAYO 2023
 def producto_matriz(matrizUno, matrizDos):
-    pass
+    filasUno, columnasUno = len(matrizUno), len(matrizUno[0])
+    filasDos, columnasDos = len(matrizDos), len(matrizDos[0])
+    if columnasUno != filasDos:
+        raise ValueError("Las matrices no se pueden multiplicar")
+    resultado = [[0 for j in range(columnasDos)] for i in range(filasUno)]
+    for i in range(filasUno):
+        for j in range(columnasDos):
+            valor = 0
+            for k in range(columnasUno):
+                valor += matrizUno[i][k] * matrizDos[k][j]
+            resultado[i][j] = valor
+    return resultado
 
 
-def resta_matriz(matrizUno, MatrizDos):
-    pass
+def resta_matriz(matrizUno, matrizDos):
+    n = len(matrizUno)
+    resultado = [[0 for j in range(n)] for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            valor = matrizUno[i][j] - matrizDos[i][j]
+            resultado[i][j] = valor
+    return resultado
 
 
-def suma_matriz(matrizUno, MatrizDos):
-    pass
+def suma_matriz(matrizUno, matrizDos):
+    n = len(matrizUno)
+    resultado = [[0 for j in range(n)] for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            valor = matrizUno[i][j] + matrizDos[i][j]
+            resultado[i][j] = valor
+    return resultado
 
 
 def determinante_matriz(matriz):
@@ -73,6 +96,18 @@ def llenado_matriz(opcion, numFil=0):
         return matriz
 
 
+def matrices_iguales(matrizUno, matrizDos):
+    filasUno, columnasUno = len(matrizUno), len(matrizUno[0])
+    filasDos, columnasDos = len(matrizDos), len(matrizDos[0])
+    if filasUno != filasDos or columnasUno != columnasDos:
+        return False
+    for i in range(filasUno):
+        for j in range(columnasUno):
+            if matrizUno[i][j] != matrizDos[i][j]:
+                return False
+    return True
+
+
 def menu():
     print("-------------------------------------------------------")
     print("PROGRAMA GENERAL DE OPERACIONES CON MATRICES")
@@ -90,22 +125,54 @@ def menu():
 def opcion_menu(opcion):
     match opcion:
         case "A":
-            print("A")
+            print("Obtencion del producto de dos matrices")
+            print("Recuerda que para que el producto sea valido es necesario que el número de columnas de la primera "
+                  "matriz sea igual al número de filas de la segunda matriz")
+            num_filas = int(input("Introduce el numero de filas que tiene la primera matriz: "))
+            matriz1 = llenado_matriz("C", num_filas)
+            num_filas2 = int(input("Introduce el numero de filas que tiene la segunda matriz: "))
+            matriz2 = llenado_matriz("C", num_filas2)
+            resultado = producto_matriz(matriz1, matriz2)
+            print("El producto de la matriz uno {} y la matriz dos {} es: {}".format(matriz1, matriz2, resultado))
         case "B":
-            print("B")
+            print("Resta de dos matrices")
+            print("Recuerda que para que la resta sea valida, ambas matrices tienen que tener el mismo numero de "
+                  "elementos")
+            num_filas = int(input("Introduce el numero de filas que tienen las matrices: "))
+            matriz1 = llenado_matriz("C", num_filas)
+            matriz2 = llenado_matriz("C", num_filas)
+            if matrices_iguales(matriz1, matriz2):
+                resultado = resta_matriz(matriz1, matriz2)
+                print("La resta de la matriz uno {} y la matriz dos {} es: {}".format(matriz1, matriz2, resultado))
+            else:
+                print("\t\t----------ERROR---------- "
+                      "\nLas matrices no tienen los mismos elementos"
+                      "\nVuelve a leer las instrucciones")
         case "C":
-            pass
+            print("Suma de dos matrices")
+            print("Recuerda que para que la suma sea valida ambas matrices tienen que tener el mismo numero de "
+                  "elementos")
+            num_filas = int(input("Introduce el numero de filas que tienen las matrices: "))
+            matriz1 = llenado_matriz("C", num_filas)
+            matriz2 = llenado_matriz("C", num_filas)
+            if matrices_iguales(matriz1, matriz2):
+                resultado = suma_matriz(matriz1, matriz2)
+                print("La suma de la matriz uno {} y la matriz dos {} es: {}".format(matriz1, matriz2, resultado))
+            else:
+                print("\t\t----------ERROR---------- "
+                      "\nLas matrices no tienen los mismos elementos"
+                      "\nVuelve a leer las instrucciones")
         case "D":
             print("Obtencion del Determinante de una matriz")
-            numFilas = int(input("Introduce el numero de filas que tiene la Matriz: "))
-            matriz = llenado_matriz("D", numFilas)
+            num_filas = int(input("Introduce el numero de filas que tiene la Matriz: "))
+            matriz = llenado_matriz("D", num_filas)
             determinante = determinante_matriz(matriz)
             print("El determinante de la matriz {} es: {}".format(matriz, determinante))
 
         case "E":
             print("Solucion de un sistema de ecuaciones de N dimensiones")
-            numFilas = int(input("Introduce el numero de filas que tiene la Matriz: "))
-            matriz, vector_resultados = llenado_matriz("E", numFilas)
+            num_filas = int(input("Introduce el numero de filas que tiene la Matriz: "))
+            matriz, vector_resultados = llenado_matriz("E", num_filas)
             print("Matriz: {}".format(matriz))
             print("Vector: {}".format(vector_resultados))
             resultado = sistema_N_Dimensiones(matriz, vector_resultados)
